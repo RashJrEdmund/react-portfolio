1b4e9c66b581ef04ed3766dd24653d9c0850315a
rash <orashusedmund@gmail.com>
2023-02-28 13:40:30 +0100

    created a json to store projects data and mapped over it

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import '../styles/projectContainer.css';
+import data from '../data/projects.json';
 
 export default function ProjectContainer() {
   return (
     <div className="project-container" id="project-container">
       <header className="project-header">Past Projects Below</header>
       <div className="container">
-        <div className="project">project 1</div>
-        <div className="project">project 2</div>
-        <div className="project">project 3</div>
-        <div className="project">project 4</div>
-        <div className="project">project 5</div>
-        <div className="project">project 6</div>
+        {data.map((piece) => {
+          return (
+            <a href={piece.link} target="_blank" rel="noreferrer">
+              <div className="project">
+                <div
+                  className="image-holder"
+                  style={{ backgroundImage: `url('${piece.img}')` }}
+                />
+                <p>
+                  <span>&#60;</span>
+                  {piece.title}
+                  <span>/&#62;</span>
+                </p>
+              </div>
+            </a>
+          );
+        })}
       </div>
     </div>
   );
